4e50c736900c066fc31ed1e386c8c025d849c739
alleny77746 <alleny77746@gmail.com>
2015-07-30 14:26:33 -0400

    mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :users
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 end
